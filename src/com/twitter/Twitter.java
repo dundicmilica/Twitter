5315e530cc92b6ed0b81e979cb896a9f4e2a980e
dundicmilica <dundic.milica94@gmail.com>
2015-03-25 19:55:51 +0100

    Napravlena JavaDoc HTML dokumentacija za javnu klasu Twitter, njene atribute i metode Folder doc sa javaDoc HTML dokumentacijom postavljen u .gitignore

--- src/com/twitter/Twitter.java
+++ src/com/twitter/Twitter.java
@@ -3,55 +3,78 @@ package com.twitter;
 import java.util.LinkedList;
 import com.twitter.poruke.TwitterPoruka;
 
+/**
+ * Predstavlja sve poruke na twitteru
+ * @author Dundic Milica
+ *
+ */
 public class Twitter {
 	
+	/**
+	 * Lista svih poruka (twit-ova) na twitteru.
+	 */
 	private LinkedList<TwitterPoruka> poruke =
 			new LinkedList<TwitterPoruka>();
-			
+	
+	/**
+	 * Metoda vraca listu svih twit-ova
+	 * @return poruke kao listu TwitterPoruka
+	 */
 	public LinkedList<TwitterPoruka> vratiSvePoruke(){
 			return poruke;
 	}
 	
+	/**
+	 * Metoda unosi novi twit na kraj liste na osnovu
+	 * ulaznih parametara
+	 * @param korisnik user name korisnika
+	 * @param poruka twit koji se objavljuje
+	 */
 	public void unesi(String korisnik, String poruka) {
-	
-		//Pravi se nova poruka i puni podacima.
 		TwitterPoruka twit = new TwitterPoruka();
 		
 		twit.setKorisnik("korisnik");
 		twit.setPoruka(poruka);
 		
-		//Poruka se unosi u listu na kraj
 		poruke.addLast(twit);
 	}
 	
-		public TwitterPoruka[] vratiPoruke(int maxBroj, String tag) {
-			if (tag==null || tag.isEmpty())
-				throw new RuntimeException("Morate uneti tag");
+	/**
+	 * Metoda proverava da li u listi TwitterPoruka postoji poruka
+	 * sa tag-om. Ako se nadje neka takva i ako nije prekoracen maxBroj
+	 * ona se upisuje u niz. Ako je prekoracen maxBroj, pretraga se prekida.
+	 * Metoda kao ulazne parametre prima 
+	 * @param maxBroj maksimalan broj poruka koje se upisuju u niz
+	 * @param tag kljucna rec pomocu koje se vrsi pretrazivanje poruka
+	 * @return TwitterPoruka[] niz od maxBroj elemenata, poruka koje sadrze tag
+	 * @throws java.lang.RuntimeException ako je uneti tag
+	 * <ul>
+	 * <li>null</li>
+	 * <li>prazanString</li>
+	 * </ul>
+	 */
+	public TwitterPoruka[] vratiPoruke(int maxBroj, String tag) {
+		if (tag==null || tag.isEmpty())
+			throw new RuntimeException("Morate uneti tag");
 	
-			//Ako je maxBroj <=0, vraca maxBroj se postavlja na 100 poruka
-			if (maxBroj<=0)
+		if (maxBroj<=0)
 				maxBroj = 100;
 			
-			//Pomocna promenljiva koja predstavlja brojac upisanih poruka
-			int brojac = 0;
-				
-			//Pomocni niz koja predstavlja rezultat pretrage tj. sadrzace
-			//sve poruke koje u sebi imaju zadati tag
-			TwitterPoruka[] rezultat = new TwitterPoruka[maxBroj];
-				
-			//Pretrazuju se poruke i traze se one koje sadrze tag.
-			//Ako se nadje neka takva, i ako nije prekoracen maxBroj
-			//ona se upisuje u niz. Ako je prekoracen maxBroj,pretraga
-			//se prekida.			
-			for (int i = 0; i < poruke.size(); i++)
-				if (poruke.get(i).getPoruka().indexOf(tag)!=-1)
-					if (brojac < maxBroj){
-						rezultat[brojac+1]=poruke.get(i);
-							brojac++;
-			}
-					else break;
-			
-			return rezultat;
+		//Pomocna promenljiva koja predstavlja brojac upisanih poruka
+		int brojac = 0;
+		
+		//Niz koji predstavlja rezultat pretrage
+		TwitterPoruka[] rezultat = new TwitterPoruka[maxBroj];
+							
+		for (int i = 0; i < poruke.size(); i++)
+			if (poruke.get(i).getPoruka().indexOf(tag)!=-1)
+				if (brojac < maxBroj){
+					rezultat[brojac+1]=poruke.get(i);
+						brojac++;
 		}
+				else break;
+		
+		return rezultat;
+	}
 			
 }
